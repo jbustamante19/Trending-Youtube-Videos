0affdbf6c450bc493b1c2c1bd42155c9ae7f482f
Bustamante <Familia@Dianas-MacBook-Pro-3.local>
2020-05-16 17:54:02 -0700

    progress

---
+++ youtubecsv/insert_data.py
@@ -0,0 +1,102 @@
+import pandas as pd
+import pymongo
+import json
+
+#DATA CLEAN UP 
+ # Save path to data set in a variable
+data_fileUS = "youtube-new/USvideos.csv"
+data_fileCA = "youtube-new/CAvideos.csv"
+
+ # Use Pandas to read data
+data_file_pdUS = pd.read_csv(data_fileUS)
+data_file_pdCA = pd.read_csv(data_fileCA)
+##data_file_pd.head()
+
+#eliminating unneccesary columns
+
+usVideosDF = data_file_pdUS[["category_id","views","likes","dislikes","comment_count","title","video_id"]]
+caVideosDF = data_file_pdCA[["category_id","views","likes","dislikes","comment_count","title","video_id"]]
+# removing some for debug : "description","trending_date",
+##usVideosDF
+
+#checking for missing data 
+
+##usVideosDF.count()
+
+#NOTE: some records are missing description values
+
+#category overview
+##usVideosDF["category_id"].value_counts()
+#NOTE: not all categories have trending videos
+
+#views overview
+
+views_summary = pd.DataFrame(usVideosDF["views"].describe())
+views_min = usVideosDF["views"].min()
+##print(views_min)
+views_max = usVideosDF["views"].max()
+##print(views_max)
+views_mean = usVideosDF["views"].mean()
+##print(views_mean)
+
+#likes overview
+
+likes_summary = pd.DataFrame(usVideosDF["likes"].describe())
+likes_min = usVideosDF["likes"].min()
+##print(likes_min)
+likes_max = usVideosDF["likes"].max()
+##print(likes_max)
+likes_mean = usVideosDF["likes"].mean()
+##print(likes_mean)
+
+#comments overview
+
+comments_summary = pd.DataFrame(usVideosDF["comment_count"].describe())
+comments_min = usVideosDF["comment_count"].min()
+##print(comments_min)
+comments_max = usVideosDF["comment_count"].max()
+##print(comments_max)
+comments_mean = usVideosDF["comment_count"].mean()
+##print(comments_mean)
+
+#date manipulation
+#dateSample = usVideosDF["trending_date"][0][0:2] +"/"+ usVideosDF["trending_date"][0][6:8]
+
+#converting DataFrame into Dictionary
+
+usVideosDict = usVideosDF.to_dict("records")
+caVideosDict = caVideosDF.to_dict("records")
+#print(usVideosDict)
+usFile = open("youtube-new/US_category_id.json",)
+usCat = json.load(usFile)
+#print(uScat)
+caFile = open("youtube-new/CA_category_id.json",)
+caCat = json.load(caFile)
+
+#DATABASE
+
+# Setup connection to mongodb
+conn = "mongodb://localhost:27017"
+client = pymongo.MongoClient(conn)
+
+# Select database and collection to use
+db = client.trendingVideos
+
+db.usVideosCollection.drop()
+db.usCatJsonCollection.drop()
+db.caVideosCollection.drop()
+db.caCatJsonCollection.drop()
+
+
+usVideosCollection = db.usVideosCollection
+usCatJsonCollection = db.usCatJsonCollection
+caVideosCollection = db.caVideosCollection
+caCatJsonCollection = db.caCatJsonCollection
+
+
+usVideosCollection.insert_many(usVideosDict)
+usCatJsonCollection.insert_one(usCat)
+caVideosCollection.insert_many(caVideosDict)
+caCatJsonCollection.insert_one(caCat)
+
+print("Data Uploaded to Database!")
